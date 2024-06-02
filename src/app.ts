b83b61dcf66a48512de72f5d2d57739ec25690bb
Phitron Batch 3 <rahatkhanpathan99@gmail.com>
2024-06-03 01:34:54 +0600

    added cors policy

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,11 @@ import apiRoutes from "./routes";
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: configs.CLIENT_URL,
+    methods: 'GET,PUT,POST,DELETE',
+    credentials: true,
+  }));
 app.use(express.json());
 
 const port = configs.PORT || 7868;
